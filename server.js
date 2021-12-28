7f04b28181aae67466d24f8918ab76d6b68f57ac
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-12-28 15:28:19 +0300

    tinder clone backend complete

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-const PORT = process.env.PORT || 5000;
+import cards from "./models/dbCards.js";
 
+const PORT = process.env.PORT || 5000;
 // app setup
 const app = express();
 
@@ -12,15 +14,45 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// database setup
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+// connection to database 
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+  })
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.log("Error connecting",err));
+
 // API routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// post data to database
+app.post("/api/cards", (req, res) => {
+  const dbCard = req.body;
+  console.log(dbCard);
+  cards.create(dbCard, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+// get data from database
+app.get("/api/cards", (req, res) => {
+  cards.find((err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
 });
 
 // listen
