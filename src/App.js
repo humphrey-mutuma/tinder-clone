3a581af4271d72ba3282ecddbd6f94ab264bc89a
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-12-28 10:58:54 +0300

    swipebuttons design complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import TinderCards from "./components/TinderCards";
 import Header from "./components/Header";
-
+import SwipeButtons from "./components/SwipeButtons";
 function App() {
   return (
     <div className="app">
       <Header />
       <TinderCards/>
+      <SwipeButtons/>
     </div>
   );
 }
